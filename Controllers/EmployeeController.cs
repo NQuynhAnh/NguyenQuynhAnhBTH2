9339c89ba8a120f11705b13fa14e189c51a8f4c6
NQuynhAnh <nguyenquynh010920@gmail.com>
2022-11-02 18:27:35 +0700

    NQA_0211

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -139,7 +139,43 @@ namespace NguyenQuynhAnh.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Upload(IFormFile file)
         {
-            
+            if(file!=null){
+                string fileExtension = Path.GetExtension(file.FileName);
+                if(fileExtension != ".xls" && fileExtension != ".xlsx")
+                {
+                    ModelState.AddModelError("", "Please choose excel file to upload!");
+                }
+                else
+                {
+                    //rename file when upload to server
+                    var fileNam = DateTime.Now.ToShortTimeString() + fileExtension;
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory() + "/Upload/Excels", fileName) ;
+                    var fileLocation = new FileInfo(filePath).ToString();
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        //save file to sever
+                        await file.CopyToAsync(stream);
+                        //read data from file and write to database
+                        var dt = _excelProcess.ExcelToDataTable(fileLocation);
+                        //using for loop to read data form dt
+                        for (int i = 0; i < dt.Rows.Count; i++)
+                        {
+                            //create a new Employee object
+                            var emp = new Employee();
+                            //set values for attribiutes
+                            emp.EmpID = dt.Rows[i][0].ToString();
+                            emp.EmpName = dt.Rows[i][1].ToString();
+                            emp.Address = dt.Rows[i][2].ToString();
+                            //add oject to context
+                            _context.Employee.Add(emp);
+                        }
+                        //save to database
+                        await _context.SaveChangesAsync();
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+                return View();
+            }
             return View();
             
         }
